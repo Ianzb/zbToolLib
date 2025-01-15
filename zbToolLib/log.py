67d64b612a878ad9c11224ace16c391cfc42542d
Ianzb <93322252@qq.com>
2025-01-15 17:00:03 +0800

    1.1.0

--- zbToolLib/log.py
+++ zbToolLib/log.py
@@ -2,7 +2,7 @@ import logging
 from sys import stderr
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 handler1 = logging.StreamHandler(stderr)
 handler1.setFormatter(logging.Formatter("[%(levelname)s %(asctime)s %(filename)s %(process)s]:%(message)s"))
