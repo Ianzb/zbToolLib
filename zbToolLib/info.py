697cae9a9ffbc9fa627da1848be9095081b2af0e
Ianzb <93322252@qq.com>
2026-02-21 12:33:44 +0800

    更新系统路径方法，不再是单独的函数。

--- zbToolLib/info.py
+++ zbToolLib/info.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import tempfile
 
 USER_PATH = os.path.expanduser("~")  # 系统用户路径
 REQUEST_HEADER = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"}  # 程序默认网络请求头
@@ -20,14 +21,9 @@ if SYSTEM_TYPE == "Windows":
     from winreg import QueryValueEx, OpenKey, HKEY_CURRENT_USER
     import ctypes
 
-
-    def DESKTOP_PATH():
-        return QueryValueEx(OpenKey(HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"), "Desktop")[0]
-
-
-    def DOWNLOAD_PATH():
-        return QueryValueEx(OpenKey(HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"), "{374DE290-123F-4565-9164-39C4925E467B}")[0]
-
+    DESKTOP_PATH = QueryValueEx(OpenKey(HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"), "Desktop")[0]
+    DOWNLOAD_PATH = QueryValueEx(OpenKey(HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"), "{374DE290-123F-4565-9164-39C4925E467B}")[0]
+    TEMP_PATH = tempfile.gettempdir()
 
     try:
         # 设置错误模式
